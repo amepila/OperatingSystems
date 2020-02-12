1aea13de1387c0c6ff0f22b4ed36db42fd97fcff
amepila <ie704453@iteso.mx>
2020-02-12 09:48:32 -0700

    Command with 1 argument ready

--- practica2/sh.c
+++ practica2/sh.c
@@ -4,18 +4,55 @@
 #include <sys/wait.h>
 
 #define LENGTH_TEXT 30
+#define ASCII_SPACE	32
 
 int main(void)
 {
 	char *argv[3] = {NULL,NULL,NULL};
 	char command[LENGTH_TEXT] = {0};
-	char temp[LENGTH_TEXT] = {0};
+	char temp1[LENGTH_TEXT] = {0};
+	char temp2[LENGTH_TEXT] = {0};
+
+	int counter1;
+	int counter2;
+	int len_cmd;
+	int flag_space;
 
 	for(;;)
 	{
 		printf("sh > ");
-		fgets(temp,LENGTH_TEXT,stdin);
-		memcpy(command,temp,strlen(temp)-1);
+		fgets(temp1,LENGTH_TEXT,stdin);
+		memcpy(command,temp1,strlen(temp1)-1);
+		memset(temp1,0,strlen(temp1));
+
+		len_cmd = strlen(command);
+		flag_space = 0;
+
+		for(counter1 = 0; counter1 <= len_cmd; counter1++)
+		{
+			if(command[counter1] == ASCII_SPACE)
+				flag_space = 1;
+		}
+
+		if(flag_space)
+		{
+			for(counter1 = 0; command[counter1] != ASCII_SPACE; counter1++)
+				temp1[counter1] = command[counter1];
+			counter1++;
+			counter2 = 0;
+
+			for(;counter1 <= len_cmd; counter1++)
+			{
+				temp2[counter2] = command[counter1];
+				counter2++;
+			}
+			memset(command,0,strlen(command));
+			memcpy(command,temp1,strlen(temp1));
+			argv[1] = temp2;
+
+			printf("%s\n", command);
+			printf("%s\n", argv[1]);
+		}
 
 		if(strcmp(command,"exit") == 0)
 		{
@@ -32,7 +69,11 @@ int main(void)
 			else
 				wait(NULL);
 		}
-		memset(temp,0,strlen(temp));
+		
+		argv[0] = NULL;
+		argv[1] = NULL;
+		memset(temp1,0,strlen(temp1));
+		memset(temp2,0,strlen(temp2));
 		memset(command,0,strlen(command));
 	}
 	return 0;
