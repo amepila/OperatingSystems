f3902ffead82d3623f9404624c3d777a601f03f7
amepila <ie704453@iteso.mx>
2020-03-01 19:53:14 -0700

    Testing

--- practica4/todo/schedulerRetro.c
+++ practica4/todo/schedulerRetro.c
@@ -1,4 +1,5 @@
 #include <scheduler.h>
+#include <stdio.h>
 
 extern THANDLER threads[MAXTHREAD];
 extern int currthread;
@@ -9,12 +10,14 @@ QUEUE ready;
 QUEUE waitinginevent[MAXTHREAD];
 QUEUE priority_q[MAXTHREAD];
 
+int status_ready = 1;
+int current_q = 0;
+
 void scheduler(int arguments)
 {
 	int old,next;
 	int changethread=0;
 	int waitingthread=0;
-	static n_ready = 1;
 	
 	int event=arguments & 0xFF00;
 	int callingthread=arguments & 0xFF;
@@ -24,7 +27,7 @@ void scheduler(int arguments)
 		// Un nuevo hilo va a la cola de listos
 		threads[callingthread].status=READY;
 		_enqueue(&priority_q[0],callingthread);
-		n_ready++;
+		status_ready++;
 	}
 	
 	if(event==BLOCKTHREAD)
@@ -40,27 +43,44 @@ void scheduler(int arguments)
 	{
 		threads[callingthread].status=END;
 		changethread=1;
-		n_ready--;
+		status_ready--;
 	}
 	
 	if(event==UNBLOCKTHREAD)
 	{
 			threads[callingthread].status=READY;
-			_enqueue(&ready,callingthread);
+			if((MAXTHREAD-1) == current_q)
+				_enqueue(&priority_q[current_q],callingthread);
+			else
+				_enqueue(&priority_q[current_q+1],callingthread);
 	}
 
 	if(event==TIMER)
 	{
-		if(thread[callingthread].status==RUNNING)
+		if(threads[callingthread].status==RUNNING)
 		{
-
+			threads[callingthread].status=READY;
+			if((status_ready ==1)||(MAXTHREAD-1) == current_q)
+				_enqueue(&priority_q[current_q],callingthread);
+			else
+				_enqueue(&priority_q[current_q+1],callingthread);
 		}
 	}
+
 	if(changethread)
 	{
+		int counter;
 		old=currthread;
-		next=_dequeue(&ready);
-		
+
+		for(counter = 0; counter < MAXTHREAD; counter++)
+		{
+			if(!_emptyq(&priority_q[counter]))
+			{
+				next=_dequeue(&priority_q[counter]);
+				current_q = counter;
+				break;
+			}
+		}
 		threads[next].status=RUNNING;
 		_swapthreads(old,next);
 	}
